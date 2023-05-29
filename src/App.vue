398e0e5e9e63388aff60411552dddb85e0be045b
nurcancurebal <nurcancurebal@hotmail.com>
2023-05-29 18:48:10 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ this.$store.state.title }}<br />
+    {{ this.$store.getters.getTitle }}<br />
+    <button @click="changeTitle()">change title</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  methods: {
+    changeTitle() {
+      // this.$store.commit("setTitle", "mutations Title");
+
+      this.$store.dispatch("setTitleActions", "actions Title");
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as getters from "./getters"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    title: "message"
   },
-  getters: {
-  },
+  getters,
   mutations: {
+    setTitle: (state, value) => {
+      return state.title = value
+    }
   },
   actions: {
+    setTitleActions: (context, value) => {
+      return context.commit("setTitle", value)
+    }
   },
   modules: {
   }
